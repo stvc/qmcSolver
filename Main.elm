2653a803aebff9e2c3c5d4a1de2f4c16d98d3e52
stvc <stevecain7@gmail.com>
2014-11-07 17:32:39 -0600

    updated for Elm 0.13 and completed basic functionality

--- Main.elm
+++ Main.elm
@@ -1,36 +1,46 @@
 import String (toInt, split, show, trim, fromList)
 import Graphics.Input (Input, input)
 import Graphics.Input.Field as Field
-import Maybe (Just, Nothing, justs)
+import List (filterMap)
+import Maybe (Maybe(..))
+import Text
 import Window
 
+-- Types
+type Minterm = Int
+type Implicant = [Maybe Bool]
+
+
+-- Signals
 minTermString : Input Field.Content
 minTermString = input Field.noContent
 
-minTerms : Signal [Int]
-minTerms = lift (sort . uniques [] . stringToListOfNums) minTermString.signal
+minTerms : Signal [Minterm]
+minTerms = lift (sort << uniques [] << stringToListOfNums) minTermString.signal
 
 dontCareString : Input Field.Content
 dontCareString = input Field.noContent
 
-dontCares' : Signal [Int]
-dontCares' = lift (sort . uniques [] . stringToListOfNums) dontCareString.signal
+dontCares' : Signal [Minterm]
+dontCares' = lift (sort << uniques [] << stringToListOfNums) dontCareString.signal
 
-dontCares : Signal [Int]
+dontCares : Signal [Minterm]
 dontCares = uniques <~ minTerms ~ dontCares'
 
--- this function might need a bit of explaination to understand what is happening.
+justs = filterMap identity
+
+-- this function might need a bit of explanation to understand what is happening.
 -- First: it takes a list as a key, and another list to filter
 -- it removes all occurences of any elements in the first list from the second,
 -- and also removes all duplicates that occure within the second list itself
 -- the result is a list of only new, unique values
 uniques : [Int] -> [Int] -> [Int]
 uniques k = let
-    removeDupes x xs = if (filter ((==) x) (k ++ xs)) == [] then x :: xs else xs in
-    foldr removeDupes []
+        removeDupes x xs = if (filter ((==) x) (k ++ xs)) == [] then x :: xs else xs
+    in foldr removeDupes []
 
 stringToListOfNums : Field.Content -> [Int]
-stringToListOfNums = justs . map (toInt . trim) . split "," . .string
+stringToListOfNums = justs << map (toInt << trim) << split "," << .string
 
 display : (Int,Int) -> Element -> Element -> [Int] -> [Int] -> Element
 display (x,y) mtf dcf mt dc = above
@@ -48,96 +58,96 @@ display (x,y) mtf dcf mt dc = above
                 <| monospace <| toText <| " DEBUG INFO:\n  Minterms: " ++ (show mt)
                     ++ "\n  DontCares: " ++ (show dc)
                     ++ "\n  NumInputs: " ++ (show <| getNumInputs (dc ++ mt))
-                    ++ "\n  Sorted Binary: " ++ (show <| map (map fromList) <| partitionByNumOnes <| decToListBinChar (getNumInputs (dc ++ mt)) (dc ++ mt))
-                    ++ "\n  Off by one test: " ++ (show <| isOffByOneTest)
-                    ++ "\n  Replacement Test: " ++ (show <| replaceDifferencesTest.exp)
-                    ++ "\n  0100 to implicant: " ++ (show <| charsToImplicant ['0','1','0','0'])
+                    ++ "\n  Implicants: " ++ (show <| mintermsToImplicants (dc ++ mt))
+                    ++ "\n  Back to minterms: " ++ (show <| concatMap implicantToMinterms <| mintermsToImplicants (dc ++ mt))
+                    ++ "\n  Reduce test: " ++ (show <| buildReducedImplicants <| mintermsToImplicants (dc ++ mt))
         ,   container x 30 middle <| centered <| Text.color lightCharcoal
-                <| toText "Written by Steven Cain"
+                <| toText "Written by Steve Cain"
         ]
     )
 
 main : Signal Element
 main = display <~ Window.dimensions ~
-    (Field.field Field.defaultStyle minTermString.handle id "MinTerms" <~ minTermString.signal) ~
-    (Field.field Field.defaultStyle dontCareString.handle id "'Dont Care' Terms" <~ dontCareString.signal) ~
+    (color white <~ (Field.field Field.defaultStyle minTermString.handle identity "MinTerms" <~ minTermString.signal)) ~
+    (color white <~ (Field.field Field.defaultStyle dontCareString.handle identity "'Dont Care' Terms" <~ dontCareString.signal)) ~
     (minTerms) ~ (dontCares)
 
-getNumInputs : [Int] -> Int
+getNumInputs : [Minterm] -> Int
 getNumInputs l = case l of
     []        -> 0
     otherwise -> ceiling <| logBase 2 <| toFloat <| (+) 1 <| maximum l
 
 -- solve takes a list of minterms, and a list of don't care terms
 -- it then combines them and converts them
-solve : [Int] -> [Int] -> String
+solve : [Minterm] -> [Minterm] -> String
 solve _ _ = "tmp"
---solve mt dc = let
---        terms = mt ++ dc
 
--- takes a bit size and a list of dec values and converts them to a list of lists of characters
-decToListBinChar : Int -> [Int] -> [[Char]]
-decToListBinChar sz = let
+
+-- Converts a minterm to an Implicant
+--   The first parameter is the size of the implicant (the number of inputs)
+mintermToImplicant : Int -> Minterm -> Implicant
+mintermToImplicant sz term = let
         convert s x = if s < 0
             then []
-            else let y = x `div` (2^(s))
+            else let y = x // (2^(s))
                 in y :: (convert (s-1) (x - y*(2^s)))
         convert' n = case n of
-            0 -> '0'
-            1 -> '1'
-    in map (map convert' . convert (sz - 1))
+            0 -> Just False
+            1 -> Just True
+    in map convert' <| convert (sz - 1) term
 
-partitionByNumOnes : [[Char]] -> [[[Char]]]
-partitionByNumOnes = let
-        countOnes = length . filter ((==) '1')
-        partByOnes n = partition ((==) n . countOnes)
-        partitionByNumOnes' n l = case l of
-            [] -> []
-            xs -> let
-                    (fpbo, spbo) = partByOnes n xs
-                in fpbo :: partitionByNumOnes' (n+1) spbo
-    in partitionByNumOnes' 0
+mintermsToImplicants : [Minterm] -> [Implicant]
+mintermsToImplicants ts = map (mintermToImplicant (getNumInputs ts)) ts
 
-type Implicant = { exp : [Char], used : Bool, terms : [Int] }
+implicantToMinterms : Implicant -> [Minterm]
+implicantToMinterms = snd << foldr implicantToMinterms' (0,[0])
 
-charsToImplicant : [Char] -> Implicant
-charsToImplicant cs = let
-        t = fst <| foldr (\ c (s, d) -> if c == '1' then (s + 2^d, d+1) else (s, d+1)) (0,0) <| cs
-    in { exp = cs, used = False, terms = [t] }
+implicantToMinterms' : Maybe Bool -> (Int, [Minterm]) -> (Int, [Minterm])
+implicantToMinterms' input (pow, terms) = let
+        fal  = terms
+        tru  = map ((+) (2 ^ pow)) terms
+        both = fal ++ tru
+    in case input of
+        Just False -> (pow + 1, fal)
+        Just True  -> (pow + 1, tru)
+        Nothing    -> (pow + 1, both)
 
--- takes two lists of implicants (the first with n 1's and the second with
--- (n+1) 1's. it returns a tuple of two lists of implicants. the first being
--- the two inputs concatenated together with their 'used' field set to true if
--- they were used, and the second being a list of reduced implicants
-generateReducedImplicants : [Implicant] -> [Implicant] -> ([Implicant], [Implicant])
-generateReducedImplicants a b = let
-        usedTerms = []
-        newTerms  = []
-    in (usedTerms, newTerms)
+offByOne : Implicant -> Implicant -> Bool
+offByOne xs ys = let
+        comb (x,y) acc = if x == y then acc else acc+1
+    in ((==) 1) <| foldl comb 0 <| zip xs ys
 
-{-
-generateMatchingImplicants : [Implicant] -> Implicant -> (Implicant, [Implicant])
-generateMatchingImplicants bs i = let
-        newTerms = foldl (\ x a -> if (isO -- shit wont work, i need to keep track of used terms in bs
-        newImplicant = if (length newTerms) == 0 then i else {i | used <- True}
-    in (newImplicant, newTerms)
+combineImplicants : Implicant -> Implicant -> Maybe Implicant
+combineImplicants a b = let
+        comb (x,y) acc = if x == y then x::acc else Nothing::acc
+        combineImplicants' xs ys = foldr comb [] <| zip xs ys
+    in if offByOne a b then Just (combineImplicants' a b) else Nothing
 
--}
+-- the first parameter is the minterm, and the second is the reduced implicant
+covers : Implicant -> Implicant -> Bool
+covers mt imp = let
+        l = zip mt imp
+        f (t,i) acc = case i of
+            Nothing   -> acc
+            otherwise -> if t == i then acc else False
+    in foldl f True l
 
-isOffByOneTest = isOffByOne (charsToImplicant ['0','0']) (charsToImplicant ['1','1'])
-replaceDifferencesTest = replaceDifferences (charsToImplicant ['1','0']) (charsToImplicant ['0','0'])
+buildReducedImplicants : [Implicant] -> [Implicant]
+buildReducedImplicants impls = let
+        buildReducedImplicants' (rImpls, nrImpls) = case rImpls of
+            []  -> nrImpls
+            otherwise -> buildReducedImplicants' (reduce rImpls) ++ nrImpls
+    in buildReducedImplicants' (impls, [])
 
-isOffByOne : Implicant -> Implicant -> Bool
-isOffByOne a b = let
-        m = a.exp
-        n = b.exp
-        isOffByOne' (x,y) c = if x == y then c else c+1
-    in if (length m) /= (length n) then False
-        else (==) 1 <| foldl isOffByOne' 0 <| zip m n
+-- reduces a list of implicants one level. returns a tuple of two lists.
+-- the first list is of reduced Implicants, the second is of unreducable implicants
+reduce : [Implicant] -> ([Implicant], [Implicant])
+reduce impl = let
+        reduce' t (rs, nrs, orig) = if (any (covers t) rs)
+            then (rs, nrs, orig)
+            else case justs (map (combineImplicants t) orig) of
+                [] -> (rs, t::nrs, orig)
+                xs -> (xs ++ rs, nrs, orig)
+        (fi,se,_) = foldl reduce' ([],[],impl) impl
+    in (fi,se)
 
-replaceDifferences : Implicant -> Implicant -> Implicant
-replaceDifferences a b = let
-        m = a.exp
-        n = b.exp
-        replaceDifferences' (x,y) acc = if x == y then x :: acc else '-' :: acc
-    in charsToImplicant <| foldr replaceDifferences' [] <| zip m n
